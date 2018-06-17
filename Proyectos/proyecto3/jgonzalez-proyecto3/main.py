497a0d06092cf02351ca0e4dac67e30a60698c39
jonDorito <jonDorito@users.noreply.github.com>
2018-06-16 23:49:01 -0600

    proyecto3

---
+++ Proyectos/proyecto3/jgonzalez-proyecto3/main.py
@@ -0,0 +1,296 @@
+import sys, pygame
+from pygame.locals import *
+from math import *
+import Car_Class
+import SavesManager
+import menus
+
+pygame.init()
+
+WIDTH  = 800
+HEIGHT = 600
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+FPS    = pygame.time.Clock()
+track_time = 15
+
+pygame.display.set_caption('road to cartaguito')
+
+track = pygame.image.load('track2.png').convert()
+track2 = pygame.image.load('track3.png').convert()
+track3 = pygame.image.load('track.png').convert()
+
+#Check if car is on track
+def on_track(sprite, pista):
+    #Color beneath the car
+    if sprite.x > 1 and sprite.x < WIDTH - 1 and sprite.y > 1 and sprite.y < HEIGHT - 1:
+        if pista.get_at((int(sprite.x), int(sprite.y))).r == 163 or pista.get_at((int(sprite.x), int(sprite.y))).r == 0 or pista.get_at((int(sprite.x), int(sprite.y))).r == 255:
+            print(True)
+            return True
+        else:
+            print(False)
+            return False
+    else:
+        pass
+#Loop del nivel 3
+def level_three(car1, car2):
+    car1.x, car1.y = (334, 524)
+    if car2 != False:
+        car2.x, car2.y = (334, 484)
+        car2.angle = 180
+        car2.third = True
+    car1.angle = 180
+    car1.third = True
+    running = True
+    t = pygame.time.get_ticks()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    menus.pause_menu()
+        seconds = (pygame.time.get_ticks() - t) / 1000
+        if seconds > track_time:
+            running = False
+        #Blit the track to the background
+        SCREEN.blit(track3, (0, 0))
+        #print(car1.self)
+
+        #Test if the game has been quit
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+        car1.move()
+        car1.wrap()
+        car1.render()
+        menus.mostrarTexto(car1.username + ' score: ' + str(car1.initial_score), 400, 300,15, menus.black)
+        if car2 != False:
+            car2.move()
+            car2.wrap()
+            car2.render()
+            menus.mostrarTexto(car2.username + ' score: ' + str(car2.initial_score), 400, 325,15, menus.black)
+        menus.mostrarTexto('Time: ' + str(int(180-seconds)), 50, 25,15, menus.red)
+
+        pygame.display.update()
+        FPS.tick(30)
+    SavesManager.leaderboard(car1.username, car1.initial_score, car1.initial_laps, car1.initial_hits)
+    if car2 != False:
+        SavesManager.leaderboard(car2.username, car2.initial_score, car2.initial_laps, car2.initial_hits)
+    running = False
+    menus.menu_principal()
+#Loop del nivel 2
+def level_two(car1, car2):
+    car1.x, car1.y = (370, 47)
+    if car2 != False:
+        car2.x, car2.y = (370, 77)
+        car2.angle = 0
+        car2.second = True
+    else:
+        car2 = False
+    car1.angle = 0
+    car1.second = True
+    running = True
+    t = pygame.time.get_ticks()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    menus.pause_menu()
+        seconds = (pygame.time.get_ticks() - t) / 1000
+        if seconds > track_time:
+            running = False
+            level_three(car1, car2)
+        #Blit the track to the background
+        SCREEN.blit(track2, (0, 0))
+        #print(car1.self)
+
+        #Test if the game has been quit
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+        car1.move()
+        car1.wrap()
+        car1.render()
+        menus.mostrarTexto(car1.username + ' score: ' + str(car1.initial_score), 400, 300,15, menus.black)
+        if car2 != False:
+            car2.move()
+            car2.wrap()
+            car2.render()
+            menus.mostrarTexto(car2.username + ' score: ' + str(car2.initial_score), 400, 325,15, menus.black)
+        menus.mostrarTexto('Time: ' + str(int(180-seconds)), 50, 25,15, menus.red)
+        pygame.display.update()
+        FPS.tick(30)
+    level_three(car1, car2)
+#Loop del nivel 1
+def level_one(car1, car2):
+    car1.x, car1.y = (53, 370)
+    if car2 != False:
+        car2.x, car2.y = (83, 370)
+        car2.angle = 90
+        car2.first = True
+    car1.angle = 90
+    car1.first = True
+    running = True
+    t = pygame.time.get_ticks()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    menus.pause_menu()
+        seconds = (pygame.time.get_ticks() - t) / 1000
+        if seconds > track_time:
+            running = False
+            level_two(car1, car2)
+        #Blit the track to the background
+        SCREEN.blit(track, (0, 0))
+        #print(car1.self)
+
+        #Test if the game has been quit
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        mouse = pygame.mouse.get_pos()
+        car1.move()
+        car1.wrap()
+        car1.render()
+        menus.mostrarTexto(car1.username + ' score: ' + str(car1.initial_score), 400, 300,15, menus.black)
+        if car2 != False:
+            car2.move()
+            car2.wrap()
+            car2.render()
+            menus.mostrarTexto(car2.username + ' score: ' + str(car2.initial_score), 400, 325,15, menus.black)
+        menus.mostrarTexto('Time: ' + str(int(180-seconds)), 50, 25,15, menus.black)
+        pygame.display.update()
+        FPS.tick(30)
+    level_two(car1, car2)
+
+#Class Car
+class Car(pygame.sprite.Sprite):
+    #Constructor car
+    def __init__(self, start_pos, start_angle, image, keys, username, initial_score, initial_laps, initial_hits):
+        '''Initialises the Car object'''
+        super().__init__()
+        self.x     = start_pos[0]
+        self.y     = start_pos[1]
+        self.angle = start_angle
+        self.speed = 0
+
+        self.image = pygame.transform.scale(pygame.image.load(image).convert_alpha(), (48, 48))
+
+        self.rotcar   = pygame.transform.rotate(self.image, self.angle)
+
+        self.keys = keys
+        self.username = username
+        self.initial_score = initial_score
+        self.initial_laps = initial_laps
+        self.initial_hits = initial_hits
+        self.check = 0
+        self.first = False
+        self.second = False
+        self.third = False
+
+    #Checks checkpoints
+    def validar_check(self, x, x2, y, angle_range1):
+        if self.x > x and self.x < x2 and self.y == y:
+            if self.check + 1 == self.goal and (self.angle > angle_range1[0] and self.angle < angle_range1[1]):
+                self.check += 1
+            else:
+                self.check -= 1
+
+    #Checks laps
+    def validar_laps(self, x, x2, y, angle_range1):
+        if self.x > x and self.x < x2 and self.y == y:
+            if self.check == self.goal and (self.angle > angle_range1[0] and self.angle < angle_range1[1]):
+                self.goal += 1
+            else:
+                self.goal -= 1
+
+    #Move car
+    def move(self, forward_speed = 1, rearward_speed = 0.2):
+        '''Moves the car when the arrow keys are pressed'''
+        keys = pygame.key.get_pressed()
+
+        #Move the car depending on which keys have been pressed
+        if keys[self.keys[0]]:
+            self.angle += self.speed
+        if keys[self.keys[1]]:
+            self.angle -= self.speed
+        if keys[self.keys[2]]:
+            self.speed += forward_speed
+        if keys[self.keys[3]]:
+            self.speed -= rearward_speed
+
+        #Keep the angle between 0 and 359 degrees
+        self.angle %= 359
+
+        #Apply friction
+        if self.first and not self.second and not self.third:
+            if on_track(self, track2): self.speed *= 0.95
+        elif self.first and self.second and not self.third:
+            if on_track(self, track3): self.speed *= 0.95
+        elif self.first and self.second and self.third:
+            if on_track(self, track): self.speed *= 0.95
+        else: self.speed *= 0.75
+
+        #Change the position of the car
+        self.x += self.speed * cos(radians(self.angle))
+        self.y -= self.speed * sin(radians(self.angle))
+
+    def wrap(self):
+        '''Wrap the car around the edges of the screen'''
+        self.wrap_around = False
+
+        if self.x <  0 :
+            self.x += WIDTH
+            self.wrap_around = True
+
+        if self.x  + self.rotcar.get_width() > WIDTH:
+            self.x -= WIDTH
+            self.wrap_around = True
+
+        if self.y  < 0:
+            self.y += HEIGHT
+            self.wrap_around = True
+
+        if self.y + self.rotcar.get_height() > HEIGHT:
+            self.y -= HEIGHT
+            self.wrap_around = True
+
+        if self.wrap_around:
+            SCREEN.blit(self.rotcar, self.rotcar.get_rect(center = (self.x, self.y)))
+
+        self.x %= WIDTH
+        self.y %= HEIGHT
+
+    def render(self):
+        '''Renders the car on the screen'''
+        self.rotcar   = pygame.transform.rotate(self.image, self.angle)
+
+        SCREEN.blit(self.rotcar, self.rotcar.get_rect(center = (self.x, self.y)))
+
+#Loop princioal del juego
+def main(user1, user2):
+    car1  = Car((0, 0), 0, 'Car1.png', [K_LEFT, K_RIGHT, K_UP, K_DOWN], user1, 20, 8, 12)
+    if user2 != False:
+        car2  = Car((0, 0), 0, 'Car.png', [K_a, K_d, K_w, K_s], user2, 12, 6, 6)
+    else:
+        car2 = user2
+    level_one(car1, car2)
+
+menus.menu_principal()
+
---
+++ Proyectos/proyecto3/jgonzalez-proyecto3/menus.py
@@ -0,0 +1,281 @@
+import pygame
+from pygame.locals import *
+from math import *
+import main
+import json
+
+WIDTH  = 800
+HEIGHT = 600
+
+
+pressed = pygame.KEYDOWN
+
+# Colores
+white = (255, 255, 255)
+red = (255, 0, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
+green = (0, 255, 0)
+darkgreen = (0, 100, 0)
+light_green = (127, 255, 212)
+
+ffx = 'freesansbold.ttf'
+
+#globals
+G_exit = False
+
+# Teclas
+
+K_2 = pygame.K_w
+K_0 = pygame.K_s
+K_1 = pygame.K_a
+K_3 = pygame.K_d
+arriba = pygame.K_UP
+abajo = pygame.K_DOWN
+derecha = pygame.K_RIGHT
+izquierda = pygame.K_LEFT
+test_x = pygame.K_s
+
+# abreviaciones
+
+pressed = pygame.KEYDOWN
+ffx = 'freesansbold.ttf'
+
+# Imagenes y configuracion de pantalla
+pantalla = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Road to Cartaguito")
+clock = pygame.time.Clock()
+Timage1 = pygame.image.load('TitleCar.png')
+PauseBack = pygame.image.load('pause_background.png')
+pause_background = pygame.transform.scale(PauseBack, (800, 600))
+Timage = pygame.transform.scale(Timage1, (int(800 * 0.6), int(600 * 0.3)))
+ScoresImg = pygame.image.load('Scores.png')
+scorepng = pygame.transform.scale(ScoresImg, (800, 600))
+IngresoBack = pygame.image.load('cr.png')
+InBack = pygame.transform.scale(IngresoBack, (800, 600))
+
+
+#music_menu = pygame.mixer.music.load('Stuff.wav')
+
+# Define la superficie y font para el texto
+def text_in_menu(text, color, font):
+    textS = font.render(text, True, color)
+    return textS, textS.get_rect()
+
+
+# Define donde se mostrara el texto y con que color y tamaño
+def mostrarTexto(text, x, y, size, color):
+    TitleText = pygame.font.Font(ffx, size)
+    TSurface, Trect = text_in_menu(text, color, TitleText)
+    Trect.center = ((x, y))
+    main.SCREEN.blit(TSurface, Trect)
+#Recibe los inputs del usernames
+def recover_input2(user1):
+    input_box = pygame.Rect(100, 100, 140, 32)
+    color_inactive = pygame.Color('lightskyblue3')
+    color_active = pygame.Color('dodgerblue2')
+    color = color_inactive
+    font = pygame.font.Font(None, 32)
+    active = False
+    text = ''
+    done = False
+    user2 = ''
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active = not active
+                else:
+                    active = False
+                # Change the current color of the input box.
+                color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        pygame.mixer.music.pause()
+                        user2 = text
+                        return main.main(user1, user2)
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+
+        pantalla.fill((30, 30, 30))
+        txt_surface = font.render(text, True, color)
+        width = max(200, txt_surface.get_width() + 10)
+        input_box.w = width
+        pantalla.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
+        pygame.draw.rect(pantalla, color, input_box, 2)
+        mostrarTexto("Insert player2's name, click the textbox to input", 200, 70, 15, white)
+
+        pygame.display.flip()
+        clock.tick(30)
+
+#Recibe los inputs del usernames
+def recover_input1jugador(player2):
+    input_box = pygame.Rect(100, 100, 140, 32)
+    color_inactive = pygame.Color('lightskyblue3')
+    color_active = pygame.Color('dodgerblue2')
+    color = color_inactive
+    font = pygame.font.Font(None, 32)
+    active = False
+    text = ''
+    done = False
+    user1 = ''
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active = not active
+                else:
+                    active = False
+                color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        user1 = text
+                        if not player2:
+                            #pygame.mixer.music.pause()
+                            return main.main(user1, False)
+                        else:
+                            recover_input2(user1)
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+
+        pantalla.fill((30, 30, 30))
+        txt_surface = font.render(text, True, color)
+        width = max(200, txt_surface.get_width() + 10)
+        input_box.w = width
+        pantalla.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
+        pygame.draw.rect(pantalla, color, input_box, 2)
+        mostrarTexto("Insert player1's name, click the textbox to input", 200, 70, 15, white)
+
+        pygame.display.flip()
+        clock.tick(30)
+
+def loopdejuego(player2):
+    return recover_input1jugador(player2)
+
+
+def ingreso(player2):
+    Ingreso = True
+    while Ingreso:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    return loopdejuego(player2)
+        pantalla.fill(white)
+        pantalla.blit(InBack, (0, 0))
+        mostrarTexto('Presione enter para continuar', (800 * 0.50), (600 * 0.40), 40, light_green)
+        mostrarTexto('Player 1 control are the W,A,S,D keys and x for shooting', (800 * 0.50), (600 * 0.55),
+                     20, light_green)
+        if player2:
+            mostrarTexto('Player 2 control are the arrow keys and space for shooting', (800 * 0.50),
+                         (600 * 0.70), 20, light_green)
+        mostrarTexto('Beta release v.0.3', (800 * 0.10), (600 * 0.95), 15, light_green)
+        clock.tick(20)
+        pygame.display.flip()
+
+# Loop del menu de scores
+def menu1():
+    with open("SaveFile.json", "r") as f:
+        datos = json.load(f)
+    m1 = True
+    s = pygame.Surface((780,580)) 
+    s.set_alpha(240)                
+    s.fill((255,255,255)) 
+    while m1:
+        for event in pygame.event.get():
+            if event.type == pressed:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+                elif event.key == test_x:
+                    m1 = False
+                    #menu_principal()
+        main.SCREEN.blit(scorepng, (0, 0))
+        #pygame.draw.rect(main.SCREEN, (245, 245, 245, 128), (10, 10, 780, 580))
+        main.SCREEN.blit(s, (0,0))
+        mostrarTexto('Username:', 66, 100, 18, black)
+        mostrarTexto('Score:', 66, 200, 18, black) 
+        mostrarTexto('Laps:', 66, 300, 18, black)
+        mostrarTexto('Hits:', 66, 400, 18, black)
+        for i in range(5):
+            mostrarTexto(str((i*1)+1) + '#', 199 + (110*i), 50, 18, black)
+        for i in range(5):
+            mostrarTexto(datos['players'][i]['username'], 199 + (110*i), 100, 18, black)
+        for i in range(5):
+            mostrarTexto(str(datos['players'][i]['total']), 199 + (110*i), 200, 18, black)
+        for i in range(5):
+            mostrarTexto(str(datos['players'][i]['laps']), 199 + (110*i), 300, 18, black)
+        for i in range(5):
+            mostrarTexto(str(datos['players'][i]['hits']), 199 + (110*i), 400, 18, black)
+        mostrarTexto('Beta release v.0.3', (800 * 0.10), (600 * 0.95), 15, light_green)
+        main.FPS.tick(20)
+        pygame.display.flip()
+
+#Menu principal
+def menu_principal():
+    #pygame.mixer.music.play(-1)
+    menu_principal = True
+    while menu_principal:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.QUIT
+                quit()
+            elif event.type == pressed:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+                elif event.key == test_x:
+                    menu1()
+                elif event.key == pygame.K_1:
+                    menu_principal = False
+                    player2 = False
+                    return ingreso(player2)
+                elif event.key == pygame.K_2:
+                    menu_principal = False
+                    player2 = True
+                    return ingreso(player2)
+        main.SCREEN.fill(yellow)
+        mostrarTexto("Road to Cartaguito", (800 * 0.5), (600 * 1 / (72 / 20)), 58, black)
+        mostrarTexto('Press 1 for solo or 2 for multiplayer', (800 * 0.5), (600 * 0.7 // 1), 28, darkgreen)
+        mostrarTexto('Press R to reload previous game', (800 * 0.5), (600 * 0.75), 26, darkgreen)
+        mostrarTexto('Press S to view scores', (800 * 0.5), (600 * 0.85), 24, darkgreen)
+        main.SCREEN.blit(Timage, (int(800 * 0.20), int(600 * 0.35)))
+        mostrarTexto('Beta release v.0.3', (800 * 0.10), (600 * 0.95), 15, darkgreen)
+        main.FPS.tick(20)
+        pygame.display.flip()
+
+#Pausa del juego
+def pause_menu():
+    pause = True
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pause = False
+                if event.key == pygame.K_m:
+                    return menu_principal()
+                if event.key == pygame.K_r:
+                    return menu_principal()
+                else:
+                    mostrarTexto('Please press either enter escape to get back to game or m to go to the menu_principal',
+                                 (800 * 0.50), (600 * 0.20), 30, red)
+        main.SCREEN.fill(white)
+        main.SCREEN.blit(pause_background, (0, 0))
+        mostrarTexto('Pause', (800 * 0.50), (600 * 0.10), 50, black)
+        mostrarTexto('Press m to got back to the menu_principal', (800 * 0.50), (600 * 0.20), 25, darkgreen)
+        mostrarTexto("Press r to restart the game in it's current mode", (800 * 0.50), (600 * 0.25), 25,
+                     darkgreen)
+        mostrarTexto("Press escape to get back into game", (800 * 0.50), (600 * 0.30), 28, green)
+        mostrarTexto('Beta release v.0.3', (800 * 0.10), (600 * 0.95), 15, light_green)
+        main.FPS.tick(20)
+        pygame.display.update()
